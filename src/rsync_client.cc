bee290da1c95d51484d0fdee1e0db7b4b37438f0
baixin <baixin@360.cn>
2024-03-15 20:29:21 +0800

    support full sync

--- src/rsync_client.cc
+++ src/rsync_client.cc
@@ -89,6 +89,10 @@ void* RsyncClient::ThreadMain() {
   }
 
   for (int i = 0; i < GetParallelNum(); i++) {
+    if (g_pika_conf->pika_model() == PIKA_CLOUD) {
+      //Waiting for interface support
+      //work_threads_[i] = std::move(std::thread(&RsyncS3Client::Copy, this, file_vec[i], i));
+    }
     work_threads_[i] = std::move(std::thread(&RsyncClient::Copy, this, file_vec[i], i));
   }
 
